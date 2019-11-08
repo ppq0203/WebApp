cac8457737358104b2d74ba07ec0aeb4cb3cc16e
Lee Jun Seop <ppq23@naver.com>
2019-11-08 09:50:53 +0900

    lab6 doing

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/testfolder/music.php');
+	header('Location: '.$uri.'/lab6/gradestore.html');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
